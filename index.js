3455837ddc9087c3f82d549d83330ed0a47eb06e
Sheetalpandey12 <sheetalpandey555@gmail.com>
2023-06-15 14:47:37 +0530

    done

--- index.js
+++ index.js
@@ -5,53 +5,68 @@ const app = express();
 app.use(express.json()); 
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root123',
-    database: 'sheetal',
-    connectionLimit: 10,
-    insecureAuth: true
+  host: 'sql12.freesqldatabase.com',
+  user: 'sql12626369',
+  password: '1zpwU4MrQR',
+  database: 'sql12626369',
+  connectionLimit: 10,
+  insecureAuth: true
 });
 
 // GET API - Retrieve all records
-app.get('/records', (req, res) => {
-    const query = `
-      SELECT r.id, r.department, r.roll, r.shift, r.brandSD, r.deviceDate, r.engineerName, r.inChargeName, i.itemName, i.unit, i.code, i.timeValue
-      FROM records AS r
-      JOIN items AS i ON r.id = i.recordId
-    `;
-    pool.query(query, (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Error retrieving records from database' });
-      } else {
-        // Group the items by record ID
-        const recordsMap = new Map();
-        results.forEach((row) => {
-          const { id, department, roll, shift, brandSD, deviceDate, engineerName, inChargeName, itemName, unit, code, timeValue } = row;
-          if (!recordsMap.has(id)) {
-            recordsMap.set(id, {
-              id,
-              department,
-              roll,
-              shift,
-              brandSD, 
-              deviceDate,
-              engineerName,
-              inChargeName,
-              items: [],
-            });
-          }
-          recordsMap.get(id).items.push({ itemName, unit, code, timeValue });
-        });
-  
-        // Convert the map values to an array
-        const records = Array.from(recordsMap.values());
-  
-        res.json(records);
-      }
-    });
+app.get("/records", (req, res) => {
+  const query = `
+    SELECT r.id, r.department, r.roll, r.shift AS recordShift, r.brandSD, r.deviceDate, r.engineerName, r.inChargeName, i.itemName, i.unit, i.code, i.shift AS itemShift, i.timeValue
+    FROM records AS r
+    JOIN items AS i ON r.id = i.recordId
+  `;
+  pool.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      res
+        .status(500)
+        .json({ error: "Error retrieving records from the database" });
+    } else {
+      const recordsMap = new Map();
+      results.forEach((row) => {
+        const {
+          id,
+          department,
+          roll,
+          brandSD,
+          deviceDate,
+          engineerName,
+          inChargeName,
+          itemName,
+          unit,
+          code,
+          recordShift,
+          itemShift,
+          timeValue,
+        } = row;
+        if (!recordsMap.has(id)) { 
+          recordsMap.set(id, {
+            id,
+            department,
+            roll,
+            shift: recordShift,
+            brandSD,
+            deviceDate,
+            engineerName,
+            inChargeName,
+            items: [],
+          });
+        }
+        recordsMap
+          .get(id)
+          .items.push({ itemName, unit, code, shift: itemShift, timeValue });
+      });
+
+      const records = Array.from(recordsMap.values());
+      res.json(records);
+    }
   });
+});
 
 // POST API - Create a new record
 app.post('/records', (req, res) => {
